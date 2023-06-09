8471482002df7a8a160c25d27d9600fcc65a0a69
Arthur <archiworking.d@gmail.com>
2023-06-09 17:26:08 +0300

    Initial Commit

---
+++ HMToasts/HMToasts.h
@@ -0,0 +1,18 @@
+//
+//  HMToasts.h
+//  HMToasts
+//
+//  Created by Артур Данилов on 09.06.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HMToasts.
+FOUNDATION_EXPORT double HMToastsVersionNumber;
+
+//! Project version string for HMToasts.
+FOUNDATION_EXPORT const unsigned char HMToastsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMToasts/PublicHeader.h>
+
+
